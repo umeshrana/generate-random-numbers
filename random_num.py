cbb37598ed4bd2a23b5be7d4573f51a7f3f5ce05
Umesh Rana <ur621799@gmail.com>
2021-04-29 16:47:27 +0200

    Add files via upload
    In this example, you will learn to generate a random number in Python.

---
+++ random_num.py
@@ -0,0 +1,28 @@
+# from random import random
+# a = random()
+# print(a)
+
+# import random
+# a = random.randint(0,10)
+# print(a)
+# a = random.randint(0,10)
+# print(a)
+# a = random.randint(0,10)
+# print(a)
+
+import random
+def random_function():
+    a = random.randint(0,100)
+    return a
+print(random_function())
+print(random_function())
+print(random_function())
+print(random_function())
+
+
+
+
+
+
+
+
